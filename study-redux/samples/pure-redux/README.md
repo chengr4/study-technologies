9a6a11de4dc8ce0348e9678268be7a6a42cec4d6
Eddie Cheng <schaffenneu47@gmail.com>
2022-02-15 17:28:26 +0800

    move all to all-about-Redux

--- study-redux/samples/pure-redux/README.md
+++
@@ -1,1 +0,0 @@
-A small example of Redux
--- study-redux/samples/pure-redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import navigationBar from './features/navigation-bar/reducer';
-
-const rootReducer = combineReducers({
-  navigationBar
-});
-
-export default rootReducer;
